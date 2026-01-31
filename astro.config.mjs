94a889b8d42744f25bb8b009d710d761d5c4b6cc
morgansnape <gabriellangeles@outlook.com>
2026-01-30 21:00:42 -0500

    Se configura para que el acceso a las rutas sea a travez de @ y no de ../ adicional se agregar el texto verdadero al condicional y se reestructura las carpetas para un código más sostenible y por ultimo se crea el Juego del número secreto

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,11 @@ export default defineConfig({
   integrations: [react(), mdx()],
 
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        '@': '/src'
+      }
+    }
   }
 });
